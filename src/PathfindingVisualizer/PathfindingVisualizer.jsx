4f48a4e236a27ae2ae2246b4b3c94751791c7479
blackpanther007007 <sharmahimanshu121663@gmail.com>
2023-08-29 00:18:04 +0530

    first commit

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -1,20 +1,72 @@
 import React, {Component} from 'react';
 import Node from './Node/Node';
 import {dijkstra, getNodesInShortestPathOrder} from '../algorithms/dijkstra';
+import {dfs, getNodespath} from '../algorithms/dfs';
+
+import styled from 'styled-components';
 
 import './PathfindingVisualizer.css';
+
+const SliderContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Value = styled.div`
+  margin-right: 10px;
+`;
+
+const SliderInput = styled.input`
+  width: 200px;
+`;
+
 
 const START_NODE_ROW = 10;
 const START_NODE_COL = 15;
 const FINISH_NODE_ROW = 10;
 const FINISH_NODE_COL = 35;
 
+const NavbarContainer = styled.nav`
+  background-color: #333;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem 2rem;
+`;
+
+const Logo = styled.a`
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: white;
+  text-decoration: none;
+`;
+
+const NavItems = styled.ul`
+  list-style: none;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`;
+
+const NavItem = styled.li`
+  margin-left: 1.5rem;
+`;
+
+const NavLink = styled.a`
+  color: white;
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
 export default class PathfindingVisualizer extends Component {
   constructor() {
     super();
     this.state = {
       grid: [],
       mouseIsPressed: false,
+      sliderValue: 5,
     };
   }
 
@@ -38,19 +90,44 @@ export default class PathfindingVisualizer extends Component {
     this.setState({mouseIsPressed: false});
   }
 
-  animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder) {
+  animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder,speed) {
+    const newspeed = parseInt(speed);
+    let x=parseInt(100);
+    const sp=newspeed;
+    x=parseInt(x/sp);
+    console.log(speed);
     for (let i = 0; i <= visitedNodesInOrder.length; i++) {
       if (i === visitedNodesInOrder.length) {
         setTimeout(() => {
           this.animateShortestPath(nodesInShortestPathOrder);
-        }, 10 * i);
+        }, x * i);
         return;
       }
       setTimeout(() => {
         const node = visitedNodesInOrder[i];
         document.getElementById(`node-${node.row}-${node.col}`).className =
           'node node-visited';
-      }, 10 * i);
+      }, x*i);
+    }
+  }
+  animateDfs(visitedNodesInOrder, nodesInShortestPathOrder,speed) {
+    const newspeed = parseInt(speed);
+    let x=parseInt(100);
+    const sp=newspeed;
+    x=parseInt(x/sp);
+    console.log(speed);
+    for (let i = 0; i <= visitedNodesInOrder.length; i++) {
+      if (i === visitedNodesInOrder.length) {
+        setTimeout(() => {
+          this.animateShortestPath(nodesInShortestPathOrder);
+        }, x * i);
+        return;
+      }
+      setTimeout(() => {
+        const node = visitedNodesInOrder[i];
+        document.getElementById(`node-${node.row}-${node.col}`).className =
+          'node node-visited';
+      }, x*i);
     }
   }
 
@@ -64,23 +141,55 @@ export default class PathfindingVisualizer extends Component {
     }
   }
 
-  visualizeDijkstra() {
+  visualizeDijkstra(speed) {
+    console.log(speed)
     const {grid} = this.state;
     const startNode = grid[START_NODE_ROW][START_NODE_COL];
     const finishNode = grid[FINISH_NODE_ROW][FINISH_NODE_COL];
     const visitedNodesInOrder = dijkstra(grid, startNode, finishNode);
     const nodesInShortestPathOrder = getNodesInShortestPathOrder(finishNode);
-    this.animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder);
+    this.animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder,speed);
   }
-
+  visualizeDfs(speed) {
+    const {grid} = this.state;
+    const startNode = grid[START_NODE_ROW][START_NODE_COL];
+    const finishNode = grid[FINISH_NODE_ROW][FINISH_NODE_COL];
+    const visitedNodesInOrder = dfs(grid, startNode, finishNode);
+    const nodesInShortestPathOrder = getNodespath(finishNode);
+    console.log(visitedNodesInOrder);
+    this.animateDfs(visitedNodesInOrder, nodesInShortestPathOrder,speed);
+  }
+  refresh() {
+    window.location.reload();
+  }
+  handleSliderChange = (event) => {
+    const newValue = parseInt(event.target.value, 10);
+    this.setState({ sliderValue: newValue });
+  };
   render() {
     const {grid, mouseIsPressed} = this.state;
-
+    const { sliderValue } = this.state;
     return (
       <>
-        <button onClick={() => this.visualizeDijkstra()}>
-          Visualize Dijkstra's Algorithm
-        </button>
+        <NavbarContainer>
+          <Logo href="/">AlgoView</Logo>
+          <NavItems>
+            <NavItem onClick={() => this.visualizeDfs(sliderValue)}>DFS</NavItem>
+            <NavItem onClick={() => this.visualizeDijkstra(sliderValue)}>Dijkstra</NavItem>
+            <NavItem onClick={() => this.refresh()}>Clear</NavItem>
+            
+          </NavItems>
+        </NavbarContainer>
+        <SliderContainer>
+        <Value>Speed: {sliderValue}</Value>
+        <SliderInput
+          type="range"
+          min={1}
+          max={15}
+          value={sliderValue}
+          onChange={(e)=>this.handleSliderChange(e)}
+        />
+      </SliderContainer>
         <div className="grid">
           {grid.map((row, rowIdx) => {
             return (
---
+++ src/algorithms/dfs.js
@@ -0,0 +1,99 @@
+// Performs Dijkstra's algorithm; returns *all* nodes in the order
+// in which they were visited. Also makes nodes point back to their
+// previous node, effectively allowing us to compute the shortest path
+// by backtracking from the finish node.
+function dfs2(grid,startNode,finishNode,visitedNodesInOrder,path)
+{   
+  
+    if(startNode.col===finishNode.col&&startNode.row===finishNode.row)
+     {
+       finishNode.isVisited=true;
+       const rt=path;
+        return [finishNode];
+     }
+     if(startNode.isWall)
+     {
+        return [];
+    }
+   if(startNode.isVisited){
+      return [] ;
+    }
+    if(finishNode.isVisited)
+    {
+        return [];
+    }
+    startNode.isVisited=true;
+   visitedNodesInOrder.push(startNode);
+    const  nb=getUnvisitedNeighbors(startNode,grid);
+    let arr=[];
+    for(let r in nb)
+    {
+        path.push(nb[r]);
+        let x=dfs2(grid,nb[r],finishNode,visitedNodesInOrder,path);
+        path.pop();
+        if(x.length>0)
+         {
+           arr.push(startNode);
+           for(let i in x)
+           {
+            arr.push(x[i]);
+           }
+           console.log(arr);
+         }
+     }
+     return arr;
+
+}
+export function dfs(grid, startNode, finishNode) {
+    const visitedNodesInOrder = [];
+    const unvisitedNodes = getAllNodes(grid);
+    const path=[];
+      console.log(startNode)
+    const allpath=dfs2(grid,startNode,finishNode,visitedNodesInOrder,path);
+    return allpath;
+  }
+  
+  function sortNodesByDistance(unvisitedNodes) {
+   
+  }
+  
+  function updateUnvisitedNeighbors(node, grid) {
+    const unvisitedNeighbors = getUnvisitedNeighbors(node, grid);
+    for (const neighbor of unvisitedNeighbors) {
+      neighbor.distance = node.distance + 1;
+      neighbor.previousNode = node;
+    }
+  }
+  
+  function getUnvisitedNeighbors(node, grid) {
+    const neighbors = [];
+    const {col, row} = node;
+    if (col < grid[0].length - 1) neighbors.push(grid[row][col + 1]);
+    if (row < grid.length - 1) neighbors.push(grid[row + 1][col]);
+    if (col > 0) neighbors.push(grid[row][col - 1]);
+    if (row > 0) neighbors.push(grid[row - 1][col]);
+    return neighbors.filter(neighbor => !neighbor.isVisited);
+  }
+  
+  function getAllNodes(grid) {
+    const nodes = [];
+    for (const row of grid) {
+      for (const node of row) {
+        nodes.push(node);
+      }
+    }
+    return nodes;
+  }
+  
+  // Backtracks from the finishNode to find the shortest path.
+  // Only works when called *after* the dijkstra method above.
+  export function getNodespath(finishNode) {
+    const nodesInShortestPathOrder = [];
+    let currentNode = finishNode;
+    while (currentNode !== null) {
+      nodesInShortestPathOrder.unshift(currentNode);
+      currentNode = currentNode.previousNode;
+    }
+    return nodesInShortestPathOrder;
+  }
+  
