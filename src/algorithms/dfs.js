67c98e0039236f9678ce6ac7f452e1b29c825731
blackpanther007007 <sharmahimanshu121663@gmail.com>
2023-08-29 01:29:37 +0530

    first commit

--- src/algorithms/dfs.js
+++ src/algorithms/dfs.js
@@ -54,10 +54,10 @@ export function dfs(grid, startNode, finishNode) {
   }
   
  
-
+  
   
   function updateUnvisitedNeighbors(node, grid) {
-    
+    const unvisitedNeighbors = getUnvisitedNeighbors(node, grid);
     for (const neighbor of unvisitedNeighbors) {
       neighbor.distance = node.distance + 1;
       neighbor.previousNode = node;
